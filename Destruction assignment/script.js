09bbdef9b42a459b4ed9598a5d4025f3f3869c54
Rajeshkumarrr <124427124+Rajeshkumarrr@users.noreply.github.com>
2023-06-08 12:15:58 +0530

    Add files via upload

---
+++ Destruction assignment/script.js
@@ -0,0 +1,105 @@
+// 01. Count the occurrences.
+
+function countWords(str) {
+    const words = str.split(" ");
+    const map = new Map();
+  
+    for (let word of words) {
+      if (map.has(word)) {
+        map.set(word, map.get(word) + 1);
+      } else {
+        map.set(word, 1);
+      }
+    }
+  
+    return map;
+  }
+  
+  const sentence = " I am learning web development from PW Skills.";
+  console.log(countWords(sentence));
+
+  
+  
+  // 02. Only unique items are allowed.
+  
+  
+  function uniqueElements(arr) {
+    return new Set(arr);
+  }
+  
+  console.log(uniqueElements([1, 2, 2, 3, 3, 4])); 
+  console.log(uniqueElements([5, 6, 7, 7, 8, 8])); 
+  
+  
+  
+  
+  
+  // 03. Swap the values.
+
+  
+  function swap(a, b) {
+    [a, b] = [b, a];
+    return [a, b];
+  }
+  
+  let x = 1;
+  let y = 2;
+  [x, y] = swap(x, y);
+  console.log(x, y);
+  
+  
+  
+  
+  
+  // 04. Access random elements.
+ 
+  function extract(arr) {
+    const [first, second, , , , last] = arr;
+    return [first, second, last];
+  }
+  
+var array = [1, 2, 3, 4, 5, 6];
+  console.log(extract(array));
+  
+  
+  
+  // 05. Min and max values.
+  
+  function maxMin(arr) {
+    return {
+      max: Math.max(...arr),
+      min: Math.min(...arr),
+    };
+  }
+  
+  const array = [3, 7, 2, 9, 4, 1];
+  console.log(maxMin(array));
+  
+  
+  
+  // 06. Nested Objects.
+  
+  
+  const person = {
+    name: "Mithun",
+    age: 21,
+    address: {
+      street: "B8, Block B, Industrial Area.",
+      city: "Sector 62, Noida",
+      state: "Uttar Pradesh",
+    },
+  };
+  
+  function extractData(obj) {
+    const {
+      name,
+      address: { street },
+    } = obj;
+    return { name, street };
+  }
+  
+  console.log(extractData(person));
+  
+  
+  
+  
